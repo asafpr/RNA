6bd1d3307e49908335efb04748df9efa18c71369
Asaf Peer <asafpr@gmail.com>
2011-06-12 12:11:01 +0300

    emma in separate function

--- conservation.py
+++ conservation.py
@@ -13,14 +13,16 @@ def average(vlist,winlen):
         retlist.append(sum(vlist[i:i+winlen-1])/float(winlen))
     return retlist
 
-def rate4site_cons(seqs,tree,onlytxid=None,winlen=13,rate4site='rate4site',emmacmd='emma'):
+def emma_MSA(seqs,alnfile=None,dndfile=None,emmacmd='emma'):
     '''
-    Compute the conservation according to rate4site. The rates for each position
-    for each sequence is returned. Gaps in the multiple alignment should
-    be taken into consideration when mapping position to evolutionary rate.
-    Assume that seqs is a dictionary with the txid as key and a sequence as
-    value. tree is a file name containing the tree in newick format 
+    Run emma (a wrapper of ClustalW from EMBOSS) on the input sequences
+    If an alnfile not assigned a temporary one will be created. The file is
+    deleted when the variable is not in use any more
     '''
+    if not alnfile:
+        alnfile = tempfile.NamedTemporaryFile()
+    if not dndfile:
+        dndfile = tempfile.NamedTemporaryFile()
     #send the sequences to multiple alignment
     #write them down
     seqfile = tempfile.NamedTemporaryFile()
@@ -31,11 +33,21 @@ def rate4site_cons(seqs,tree,onlytxid=None,winlen=13,rate4site='rate4site',emmac
         elif isinstance(seqs[txid],SeqRecord):
             seqfile.write(str(seqs[txid].seq)+'\n')
     seqfile.flush()
-    #run emma
-    alnfile = tempfile.NamedTemporaryFile()
-    dndfile = tempfile.NamedTemporaryFile()
     os.system(emmacmd+' -sequence '+seqfile.name+' -outseq '+alnfile.name+
               ' -dendoutfile '+dndfile.name+' >& /dev/null')
+
+    return alnfile
+
+def rate4site_cons(seqs,tree,onlytxid=None,winlen=13,rate4site='rate4site',emmacmd='emma'):
+    '''
+    Compute the conservation according to rate4site. The rates for each position
+    for each sequence is returned. Gaps in the multiple alignment should
+    be taken into consideration when mapping position to evolutionary rate.
+    Assume that seqs is a dictionary with the txid as key and a sequence as
+    value. tree is a file name containing the tree in newick format 
+    '''
+    #send the sequences to multiple alignment
+    alnfile = emmaMSA(seqs,emmacmd=emmacmd)
     #call Rate4Site
     grate=Rate4Site(alnfile.name,tree)
     if onlytxid:
